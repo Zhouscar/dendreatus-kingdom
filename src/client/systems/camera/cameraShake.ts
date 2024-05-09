bf8a02b28672cf4101b24e12c9f175d1cbe8e532
Zhouscar <90882182+Zhouscar@users.noreply.github.com>
2024-05-09 13:19:28 -0400

    bit more touches

--- src/client/systems/camera/cameraShake.ts
+++ src/client/systems/camera/cameraShake.ts
@@ -1,15 +1,25 @@
 import { useDeltaTime, World } from "@rbxts/matter";
 import { Damage } from "shared/components/health";
+import { CrashLanding } from "shared/components/movements";
 import { isLocalPlr } from "shared/hooks/components";
 import { State } from "shared/state";
 
-function cameraShakeOnDamage(w: World, s: State) {
+function cameraShake(w: World, s: State) {
     s.cameraShake -= useDeltaTime() / 2;
     s.cameraShake = math.clamp(s.cameraShake, 0, 1);
 
+    for (const [e, crashLandingRecord] of w.queryChanged(CrashLanding)) {
+        if (!w.contains(e)) continue;
+        if (crashLandingRecord.new === undefined) continue;
+        if (!isLocalPlr(w, e)) continue;
+
+        s.cameraShake += 0.8;
+        s.cameraShake = math.clamp(s.cameraShake, 0, 1);
+    }
+
     for (const [e, damageRecord] of w.queryChanged(Damage)) {
         if (!w.contains(e)) continue;
-        if (!damageRecord.new) continue;
+        if (damageRecord.new === undefined) continue;
         if (!isLocalPlr(w, e)) continue;
 
         if (damageRecord.new.damageType !== "physical") continue;
@@ -19,4 +29,4 @@ function cameraShakeOnDamage(w: World, s: State) {
     }
 }
 
-export = cameraShakeOnDamage;
+export = cameraShake;
