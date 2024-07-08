37f4e033a591739b0297d4698d2230cb1e2ebd66
Zhouscar <brainofnothinhness@gmail.com>
2024-07-08 00:28:28 -0400

    some more fixing

--- src/shared/systems/removeMissingModels.ts
+++ src/shared/systems/removeMissingModels.ts
@@ -1,11 +1,14 @@
 import { World, useEvent } from "@rbxts/matter";
 import { Workspace } from "@rbxts/services";
-import { Renderable } from "shared/components";
+import { Renderable, ServerRenderable } from "shared/components";
+import { hasComponents } from "shared/hooks/components";
+import { HOST } from "shared/host";
 
 function removeMissingModels(w: World): void {
     for (const [e, renderable] of w.query(Renderable)) {
         for (const _ of useEvent(renderable.pv, "AncestryChanged")) {
             if (!renderable.pv.IsDescendantOf(Workspace)) {
+                if (HOST === "CLIENT" && hasComponents(w, e, ServerRenderable)) continue;
                 w.despawn(e);
                 break;
             }
